557481ba25af233e755d2c5aa1b6784cdbc86ddf
liushuchun <liuscgood@gmail.com>
2017-05-13 22:23:37 +0800

    决策树构建

--- dtree/tree_test.py
+++ dtree/tree_test.py
@@ -19,6 +19,28 @@ class TestTrees(unittest.TestCase):
 
         data, label = trees.create_data()
         print trees.calc_shannon_ent(data)
+        ret = trees.split_data_set(data, 0, 1)
+        print ret
+
+    def test_choose_best_features(self):
+        data, label = trees.create_data()
+        self.assertEqual(trees.choose_best_feature_tosplit(data), 0)
+
+    def test_create_tree(self):
+        data, label = trees.create_data()
+
+        myTree = trees.create_tree(data, label)
+
+        print myTree
+
+    def test_classify(self):
+        data, label = trees.create_data()
+        myTree = trees.retrieve_trees(0)
+
+        print "here"
+        print myTree
+        self.assertEqual('no', trees.classify(myTree, label, [1, 0]))
+        self.assertEqual('yes', trees.classify(myTree, label, [1, 1]))
 
 
 if __name__ == "__main__":
--- dtree/trees.py
+++ dtree/trees.py
@@ -38,7 +38,7 @@ def split_data_set(data_set, axis, value):
     axis:划分的数据集的特征
     value:需要返回的特征值
     '''
-    ret_data_set = [] #防止对原有的list产生影响
+    ret_data_set = []  # 防止对原有的list产生影响
     for vec in data_set:
         if vec[axis] == value:
             reduced_vec = vec[:axis]
@@ -46,3 +46,95 @@ def split_data_set(data_set, axis, value):
             ret_data_set.append(reduced_vec)
 
     return ret_data_set
+
+
+def choose_best_feature_tosplit(data_set):
+    num_features = len(data_set[0]) - 1
+    base_entropy = calc_shannon_ent(data_set)
+    best_info_gain = 0.0
+    best_feature = -1
+    for i in range(num_features):
+        feature_list = [example[i] for example in data_set]
+        unique_vals = set(feature_list)
+        new_entropy = 0.0
+        for value in unique_vals:
+            sub_data_set = split_data_set(data_set, i, value)
+            prob = len(sub_data_set) / float(len(data_set))
+            new_entropy += prob * calc_shannon_ent(sub_data_set)
+
+        info_gain = base_entropy - new_entropy
+        if(info_gain) > best_info_gain:
+            best_info_gain = info_gain
+            best_feature = i
+
+    return best_feature
+
+
+def major_cnt(class_list):
+    class_count = {}
+    for vote in class_list:
+        if vote not in class_count.keys():
+            class_count[vote] = 0
+
+        class_count[vote] += 1
+
+        sorted_class_count = sorted(
+            class_count.iteritems, key=operator.itemgetter(1), reverse=True)
+
+        return sorted_class_count[0][0]
+
+
+def create_tree(data_set, labels):
+    class_list = [example[-1] for example in data_set]
+    if class_list.count(class_list[0]) == len(class_list):  # 类别完全相同，则停止继续划分
+        return class_list[0]
+
+    if len(data_set[0]) == 1:  # 遍历完所有特征时返回出现次数最多的
+        return major_cnt(class_list)
+
+    best_feature = choose_best_feature_tosplit(data_set)
+    besf_feature_label = labels[best_feature]
+    my_tree = {besf_feature_label: {}}
+    del(labels[best_feature])
+    feature_values = [example[best_feature]
+                      for example in data_set]  # 得到列表包含的所有属性值
+
+    unique_vals = set(feature_values)
+    for value in unique_vals:
+        sub_labels = labels[:]
+        my_tree[besf_feature_label][value] = create_tree(
+            split_data_set(data_set, best_feature, value), sub_labels)
+
+    return my_tree
+
+
+def retrieve_trees(i):
+    list_of_trees = [{
+        'no surfacing': {0: 'no', 1: {'flippers':
+                                      {
+                                          0: 'no', 1: 'yes'}}}},
+                     {
+        'no surfacing': {
+            0: 'no', 1: {
+                'flippers': {
+                    0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'
+                }
+            }
+        }
+    }]
+
+
+def classify(input_tree, feature_labels, test_vec):
+    print input_tree
+    keys = input_tree.keys()
+    first_str = keys[0]
+    second_dict = input_tree[first_str]
+    feature_index = feature_labels.index(first_str)
+    for key in second_dict.keys():
+        if test_vec[feature_index] == key:
+            if type(second_dict[key]).__name__ == 'dict':
+                class_label = classify(
+                    second_dict[key], feature_labels, test_vec)
+            else:
+                class_label = second_dict[key]
+    return class_label
